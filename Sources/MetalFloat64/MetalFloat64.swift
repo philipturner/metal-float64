543c7e9bacf077ebf3f5d7010dc5e7a79870de16
Philip Turner <philipturner.AR@gmail.com>
2022-08-14 17:41:48 -0400

    Update

---
+++ Sources/MetalFloat64/MetalFloat64.swift
@@ -0,0 +1,6 @@
+public struct MetalFloat64 {
+    public private(set) var text = "Hello, World!"
+
+    public init() {
+    }
+}
---
+++ Tests/MetalFloat64Tests/MetalFloat64Tests.swift
@@ -0,0 +1,11 @@
+import XCTest
+@testable import MetalFloat64
+
+final class MetalFloat64Tests: XCTestCase {
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct
+        // results.
+        XCTAssertEqual(MetalFloat64().text, "Hello, World!")
+    }
+}
